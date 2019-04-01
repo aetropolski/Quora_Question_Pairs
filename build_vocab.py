a3e7c0e68b0b883ca1297c3f0d7dcc672fe40306
Anastassia Etropolski <aetropolski@gmail.com>
2019-04-01 15:50:55 -0500

    add build_vocab and process_and_train files

---
+++ build_vocab.py
@@ -0,0 +1,108 @@
+import re
+import argparse
+from tensorflow.keras.preprocessing.text import Tokenizer
+import pandas as pd
+import numpy as np
+import json
+import os
+
+def normalize_text(text):
+    text = str(text)
+    text = text.lower()
+    text = re.sub(r"[^a-z0-9^/=+-]", " ", text)
+    
+    # Mathematical notation
+    text = re.sub(r"/", " / ", text)
+    text = re.sub(r"\^", " ^ ", text)
+    text = re.sub(r"\+", " + ", text)
+    text = re.sub(r"\-", " - ", text)
+    text = re.sub(r"\=", " = ", text)
+    text = re.sub(r"(\d)x(\d)","\g<1> x \g<2>",text)
+
+    return text.split()
+
+if __name__ == '__main__':
+    data = pd.read_csv('quora_duplicate_questions.tsv', sep='\t')
+    data.dropna(inplace=True)
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model", help="The embedding model you wish to use. Allowed values are glove, w2v, trainable", choices=['glove','w2v','trainable'])
+    parser.add_argument("--dimension", help="embedding dimension", type=int)
+    args = parser.parse_args()
+    
+    if args.dimension is not None:
+        embedding_dim = args.dimension
+    else:
+        embedding_dim = 300
+
+    if args.model == 'glove' and embedding_dim not in [50, 100, 200, 300]:
+        parser.error("Permissible embedding dimensions for GloVe are 50, 100, 200, or 300")
+        
+    print('Using {} model with embedding dimension {}.'.format(args.model, embedding_dim), flush=True)
+    
+    if args.model == 'w2v':
+        try:
+            from gensim.models import Word2Vec
+        except ImportError:
+            "The gensim package must be installed in order to use this model"
+        print('Compiling documents...', flush=True)
+        questions = []
+        for index, row in data.iterrows():
+            questions.append(row['question1'])
+            questions.append(row['question2'])
+        unique_questions = list(set(questions))
+        docs = [normalize_text(q) for q in unique_questions]
+        print('Training w2v model...', flush=True)
+        model = Word2Vec(docs, min_count = 5, size = embedding_dim)
+        model.train(docs, total_examples=len(docs), epochs=10)
+        print('Saving w2v model...', flush=True)
+        filename = str(embedding_dim) + 'dim_w2v_model.bin'
+        filepath = os.path.join('data','w2v',filename)
+        model.save(filepath)
+    
+    else:
+        word_data = []
+        for index, row in data.iterrows():
+            question1_words = normalize_text(row['question1'])
+            question2_words = normalize_text(row['question2'])
+            row_words = list(set(question1_words + question2_words))
+            word_data += row_words
+        unique_words = set(word_data) # keep as set for hashing
+        vocab_size = len(unique_words)
+        
+        if args.model == 'glove':
+            word_index = {}
+            embedding_matrix = np.random.normal(scale=0.6, size=(vocab_size,embedding_dim))
+            filepath = 'glove.6B/glove.6B.' + str(embedding_dim) + 'd.txt'
+            
+            with open(filepath, encoding='utf8') as f:
+                for line in f:
+                    word, *vector = line.split()
+                    if word in unique_words:
+                        idx = len(word_index) + 1
+                        word_index[word] = idx
+                        embedding_matrix[idx] = np.array(vector, dtype=np.float32)
+            
+            print('Saving pretrained glove model...', flush=True)
+            filename = str(embedding_dim) + 'dim_glove_word_index.json'
+            filepath = os.path.join('data','glove',filename)     
+            with open(filepath,'w') as write_file:
+                json.dump(word_index, write_file)
+
+            vocab_size = len(word_index) + 1
+
+            embedding_matrix = np.vstack((np.zeros((1, embedding_dim)), embedding_matrix[:vocab_size])) # first row for zero padding, final row for oov words
+            
+            filename = str(embedding_dim) + 'dim_glove_emb_matrix'
+            filepath = os.path.join('data','glove',filename)
+            
+            np.save(filepath, embedding_matrix)
+        
+        if args.model == 'trainable':
+            t = Tokenizer(filters=[])
+            t.fit_on_texts(unique_words)
+            filepath = os.path.join('data', 'trainable', 'word_index.json')
+            with open(filepath,'w') as write_file:
+                json.dump(t.word_index, write_file)
+    
+    
---
+++ process_and_train.py
@@ -0,0 +1,115 @@
+import numpy as np
+import pandas as pd
+from build_vocab import normalize_text
+from model_architecture import build_model
+import os
+import json
+import argparse
+import itertools
+
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+
+def text_to_sequence(seq_of_words, word_to_int):
+    """ Returns a sequence of words as a sequence of integers according to a provided dictionary. """
+    vocab_size = len(word_to_int) + 1
+    text_as_ints = []
+    for word in seq_of_words:
+        index = word_to_int.get(word)
+        if index == None:
+            text_as_ints.append(vocab_size)
+        else:
+            text_as_ints.append(index)
+    return text_as_ints
+    
+if __name__ == '__main__':
+    data = pd.read_csv('quora_duplicate_questions.tsv', sep='\t')
+    data.dropna(inplace=True)
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model", help="The embedding model you wish to use. Allowed values are glove, w2v, trainable", choices=['glove','w2v','trainable'])
+    parser.add_argument("--dimension", help="embedding dimension", type=int)    
+    parser.add_argument("--epochs", help="number of training epochs", type=int)
+    args = parser.parse_args()
+    
+    if args.dimension is not None:
+        embedding_dim = args.dimension
+    else:
+        embedding_dim = 300
+    
+    if args.model == 'w2v':
+        try:
+            from gensim.models import Word2Vec
+        except ImportError:
+            "The gensim package must be installed in order to use this model"
+        print('Loading word vectors...', flush=True)
+        model_file = str(embedding_dim) + "dim_w2v_model.bin"
+        model_path = os.path.join('data','w2v',model_file)
+        assert os.path.isfile(model_path), "Run build_vocab.py with w2v argument and embedding dimension {} to initialize w2v model.".format(embedding_dim)
+        model = Word2Vec.load(model_path)
+        word_to_int = {w : model.wv.vocab.get(w).index + 1 for w in model.wv.vocab}
+    if args.model == 'glove':
+        print('Loading vocabulary...', flush=True)
+        vocab_file = str(embedding_dim) + "dim_glove_word_index.json"
+        vocab_path = os.path.join('data','glove',vocab_file)
+        assert os.path.isfile(vocab_path), "Run build_vocab.py with glove argument and embedding dimension {} to initialize glove vocabulary.".format(embedding_dim)
+        with open(vocab_path, 'r') as read_file:
+            word_to_int = json.load(read_file)
+    if args.model == 'trainable':
+        print('Loading vocabulary...', flush=True)
+        vocab_path = os.path.join('data','trainable','word_index.json')
+        assert os.path.isfile(vocab_path), "Run build_vocab.py with trainable argument to initialize vocabulary for trainable embedding model."
+        with open(vocab_path, 'r') as read_file:
+            word_to_int = json.load(read_file)
+    
+    print('Encoding text...', flush=True)
+    for index, row in data.iterrows():
+        question1_words = normalize_text(row['question1'])
+        question2_words = normalize_text(row['question2'])
+        data.at[index,'question1'] = text_to_sequence(question1_words, word_to_int)
+        data.at[index,'question2'] = text_to_sequence(question2_words, word_to_int)
+
+    X = data[['question1','question2']]
+    Y = data['is_duplicate']
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.2, random_state=42)
+
+    X_train = {'left': X_train.question1, 'right': X_train.question2}
+    X_test = {'left': X_test.question1, 'right': X_test.question2}
+
+    # Make sure that both datasets are padded to the same length
+    max_seq_length = max(data['question1'].map(lambda x: len(x)).max(), data['question2'].map(lambda x: len(x)).max())
+
+    print('Padding sequences...', flush=True)
+
+    for dataset, side in itertools.product([X_train, X_test], ['left', 'right']): 
+        dataset[side] = pad_sequences(dataset[side], maxlen=max_seq_length)
+        
+    vocab_size = len(word_to_int) + 1
+    
+    if args.model == 'w2v':
+        embedding_matrix = np.vstack((np.zeros((1, embedding_dim)), model.wv.vectors, np.random.normal(scale=0.6, size=(1,embedding_dim))))
+    if args.model == 'glove':
+        emb_file = str(embedding_dim) + "dim_glove_emb_matrix.npy"
+        emb_path = os.path.join('data','glove',emb_file)
+        embedding_matrix = np.load(emb_path)
+    if args.model == 'trainable':
+        embedding_matrix = None
+        vocab_size = vocab_size - 1 # this model has no oov words
+        
+    model = build_model(vocab_size = vocab_size + 1, input_size = max_seq_length, embedding_dim = embedding_dim, lstm_units = 50, embedding_matrix = embedding_matrix, learning_rate = 0.001, clip_norm = None)
+
+    if args.epochs is not None:
+        epochs = args.epochs
+    else:
+        epochs = 10
+    batch_size = 64
+
+    checkpoint_path = os.path.join("models", args.model, "cp.ckpt")
+
+    cp_callback = ModelCheckpoint(checkpoint_path, monitor='val_acc', save_weights_only=True, save_best_only=True, mode='max', verbose=1)
+
+    model_trained = model.fit([X_train['left'], X_train['right']], Y_train, batch_size = batch_size, epochs = epochs, validation_split = 0.1, callbacks = [cp_callback], verbose=2)
+    
+    # Load best weights and print score?
